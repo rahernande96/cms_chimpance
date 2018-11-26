f1b5774faaf62d37fd74ed64a03369760546f5c2
Robert Hernandez <34660668+rahernande96@users.noreply.github.com>
2018-11-26 14:45:35 -0400

    Update geografico.blade.php

--- resources/views/geografico.blade.php
+++ resources/views/geografico.blade.php
@@ -36,7 +36,7 @@
         <div class="map-container">
             <div id="map"></div>
             <div id="map-caribe"></div>
-            <button type="button" id="cambiar" >Volver</button>
+            <button type="button" id="cambiar" >@lang('geographical.btn_return')</button>
             <div id="dialog" title="titulo pais">
                 <p></p>
             </div>
